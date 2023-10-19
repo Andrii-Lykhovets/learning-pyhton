a3c2caf62da6c810a437f56696b0c713c57213ed
Andrii-Lykhovets <alivetshoff@gmail.com>
2023-10-19 19:51:15 +0200

    before debugging

---
+++ algorithms/haircut_company.py
@@ -0,0 +1,89 @@
+print('Welcome to our salon!')
+
+
+def select_salon_services():
+    print('Enter a service that you\'d like: haircut, hair coloring, manicure, head massage? '
+          'Type stop to finish an order:')
+    giving_service = True
+    services = list()
+    while giving_service:
+        chosen_service = input()
+        if chosen_service == 'stop':
+            giving_service = False
+        else:
+            services.append(chosen_service)
+    return services
+
+
+def service_confirmation(services):
+    print('Your service is: ' + str(services))
+
+
+def haircut_tutorial():
+    haircut_type = input('How would you like to cut your hair? Long or short:')
+    if input == 'short':
+        print('1. Shorten the hair on the sides.')
+        print('2. Spray the upper hair with water and cut it with scissors and comb.')
+        print('3. Wash the head and style the hair.')
+    if input == 'long':
+        print('1. Brush the hair and spray it with water.')
+        print('2. Thoroughly cut the ends of hair.')
+        print('3. Dry the hair and style it.')
+    return haircut_type
+
+
+def coloring_tutorial():
+    print('What color would you like to dye your hair in: blond, red or black?')
+    hair_color = input()
+    if input == 'blond':
+        print('1. Bleach the hair for 30 minutes.')
+        print('2. Rinse the hair properly.')
+        print('3. Put the' + str(input) + 'toner on the hair and keep it for 10 minutes.')
+        print('4. Rinse the hair, dry, and style it.')
+    if input == 'red':
+        print('1. Bleach the hair for 30 minutes.')
+        print('2. Rinse the hair properly.')
+        print('3. Put the' + str(input) + 'toner on the hair and keep it for 10 minutes.')
+        print('4. Rinse the hair, dry, and style it.')
+    if input == 'black':
+        print('1. Bleach the hair for 30 minutes.')
+        print('2. Rinse the hair properly.')
+        print('3. Put the' + str(input) + 'toner on the hair and keep it for 10 minutes.')
+        print('4. Rinse the hair, dry, and style it.')
+    print('Here you are! Now your hair is' + str(input))
+    return hair_color
+
+
+def manicure_tutorial(nails_length, polish_color):
+    print('1. Cut the nails' + nails_length + '.')
+    print('2. Cover them with' + polish_color + ' polish.')
+    if nails_length == 'short':
+        return str(nails_length) + str(polish_color) + ' manicure.'
+    if nails_length == 'long':
+        return str(nails_length) + str(polish_color) + ' manicure.'
+
+
+def giving_service(service_list):
+    given_service = list()
+    for chosen_service in service_list:
+        given_service.append(giving_chosen_service(chosen_service))
+    return given_service
+
+
+def giving_chosen_service(chosen_service):
+    if chosen_service == 'haircut':
+        return haircut_tutorial()
+    elif chosen_service == 'hair coloring':
+        return coloring_tutorial()
+    elif chosen_service == 'manicure':
+        return manicure_tutorial()
+    else:
+        print('Giving you a nice ' + chosen_service)
+        exit()
+
+
+select_salon_services()
+ordering_a_service = select_salon_services()
+service_confirmation(ordering_a_service)
+results = giving_service(ordering_a_service)
+print('Here is your order ' + str(results))
