2c8d2264ab56bd1d1861db8b3472e32ad923b1ea
Sasha Bondarev <5994478+alex-d-bondarev@users.noreply.github.com>
2023-11-20 21:22:11 +0000

    Init tests via terminal

--- tests/test_caffe.py
+++ tests/test_caffe.py
@@ -1,42 +1,51 @@
+import pytest
+
 from algorithms.sep_oct.caffe_functions import boil_eggs, cook_an_order, sandwich_recipe
 
 
+@pytest.mark.caffe
 def test_one_hard_egg():
     actual = boil_eggs(yolk_state='hard', eggs_quantity='1')
     expected = '1 hard egg(s)'
     assert actual == expected, 'should boil "1 hard egg"'
 
 
+@pytest.mark.caffe
 def test_4_soft_eggs():
     actual = boil_eggs(yolk_state='soft', eggs_quantity='4')
     expected = '4 soft egg(s)'
     assert actual == expected
 
 
+@pytest.mark.caffe
 def test_4_liquid_eggs():
     actual = boil_eggs(yolk_state='liquid', eggs_quantity='4')
     expected = '4 liquid egg(s)'
     assert actual == expected
 
 
+@pytest.mark.caffe
 def test_negative_quantity():
     actual = boil_eggs(yolk_state='liquid', eggs_quantity='-2')
     expected = 'error, eggs quantity should be a positive number'
     assert actual == expected
 
 
+@pytest.mark.caffe
 def test_zero_eggs():
     actual = boil_eggs(yolk_state='liquid', eggs_quantity='0')
     expected = '0 liquid egg(s)'
     assert actual == expected
 
 
+@pytest.mark.caffe
 def test_cooking_function():
     actual = cook_an_order('boiled egg')
     expected = '3 hard egg(s)'
     assert actual == expected
 
 
+@pytest.mark.caffe
 def test_sandwich_function():
     actual = sandwich_recipe(amount_of_sandwiches='2', stuffing='tuna', pickles_quantity='4')
     expected = '2 sandwich(es) with tuna and 4 pickles'
--- tests/test_small_functions.py
+++ tests/test_small_functions.py
@@ -1,3 +1,5 @@
+import pytest
+
 from algorithms.sep_oct.only_first_letters_are_capitalized import only_first_letters_are_capitalized
 from algorithms.sep_oct.print_first_2_parts import print_first_2_parts
 from algorithms.sep_oct.good_flat_price import good_flat_price
@@ -6,12 +8,14 @@ from algorithms.sep_oct.calculator import calculate
 # Tests for only_first_letters_are_capitalized
 
 
+@pytest.mark.first_letters
 def test_no_changes():
     journal_text = 'This Perfect Sentence Will Not Change'
     actual = only_first_letters_are_capitalized(text=journal_text)
     assert actual == journal_text
 
 
+@pytest.mark.first_letters
 def test_capitalized():
     song = 'oLD MacDonald HAD a farm'
     expected = "Old Macdonald Had A Farm"
@@ -21,6 +25,7 @@ def test_capitalized():
 # Tests for print_first_2_parts
 
 
+@pytest.mark.first_parts
 def test_song():
     song = 'Old MacDonald had a farm Ee i ee i o'
     expected = "Old MacDonald"
@@ -28,6 +33,7 @@ def test_song():
     assert actual == expected
 
 
+@pytest.mark.first_parts
 def test_journal_text():
     journal_text = 'On October 3, 2015, Jeff Smith, marketing director at Intel, traveled to 14 Appian Way in Rome, Italy.'
     expected = "On October 3 2015"
@@ -38,18 +44,21 @@ def test_journal_text():
 # Tests for good_flat_price
 
 
+@pytest.mark.flat_price
 def test_deal1():
     actual = good_flat_price(1700, 15, 1500, 17)
     expected = "The second flat is a better deal."
     assert actual == expected
 
 
+@pytest.mark.flat_price
 def test_deal2():
     actual = good_flat_price(1500, 17, 1700, 15)
     expected = "The first flat is a better deal."
     assert actual == expected
 
 
+@pytest.mark.flat_price
 def test_deal3():
     actual = good_flat_price(1000, 33, 1000, 33)
     expected = "Both flats have the same cost."
@@ -59,24 +68,28 @@ def test_deal3():
 # Tests for calculator
 
 
+@pytest.mark.calc
 def test_add():
     actual = calculate(number1=10, number2=2, operation='add')
     expected = 12
     assert actual == expected
 
 
+@pytest.mark.calc
 def test_subtract():
     actual = calculate(number1=10, number2=2, operation='subtract')
     expected = 8
     assert actual == expected
 
 
+@pytest.mark.calc
 def test_multiply():
     actual = calculate(number1=10, number2=2, operation='multiply')
     expected = 20
     assert actual == expected
 
 
+@pytest.mark.calc
 def test_divide():
     actual = calculate(number1=10, number2=2, operation='divide')
     expected = 5
